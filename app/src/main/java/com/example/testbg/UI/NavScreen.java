9d55f0f61f935733836e4d5e7410f496983da33a
ShemBrooklyne <shemnyatti@gmail.com>
2021-03-24 18:28:31 +0300

    Missing constraints* widgets collapses

--- app/src/main/java/com/example/testbg/UI/NavScreen.java
+++ app/src/main/java/com/example/testbg/UI/NavScreen.java
@@ -1,13 +1,23 @@
 package com.example.testbg.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.testbg.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+import com.synnapps.carouselview.ViewListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,11 +31,34 @@ public class NavScreen extends AppCompatActivity implements View.OnClickListener
     @BindView(R.id.healthCard) CardView mHealth;
     @BindView(R.id.businessCard) CardView mBusiness;
 
+
+    CarouselView customCarouselView;
+
+
+    int[] sampleImages = {
+            R.drawable.carousel1,
+            R.drawable.carousel2,
+            R.drawable.carousel3,
+            R.drawable.carousel4,
+            R.drawable.carousel5,
+            R.drawable.carousel6
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_screen);
         ButterKnife.bind(this);
+
+//        ActionBar actionBar = getActionBar();
+//        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#93E9FA"));
+//        actionBar.setBackgroundDrawable(colorDrawable);
+
+        customCarouselView = (CarouselView) findViewById(R.id.customCarouselView);
+        customCarouselView.setPageCount(sampleImages.length);
+        customCarouselView.setSlideInterval(4000);
+        customCarouselView.setViewListener(viewListener);
+
 
         mLocal.setOnClickListener(this);
         mTech.setOnClickListener(this);
@@ -34,6 +67,22 @@ public class NavScreen extends AppCompatActivity implements View.OnClickListener
         mHealth.setOnClickListener(this);
         mBusiness.setOnClickListener(this);
     }
+
+    // To set custom views
+    ViewListener viewListener = new ViewListener() {
+        @Override
+        public View setViewForPosition(int position) {
+
+            View customView = getLayoutInflater().inflate(R.layout.view_custom, null);
+
+            ImageView IntroImageView = (ImageView) customView.findViewById(R.id.intoImageView);
+
+
+            IntroImageView.setImageResource(sampleImages[position]);
+
+            return customView;
+        }
+    };
 
     @Override
     public void onClick(View view) {
